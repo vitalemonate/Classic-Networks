cb5dad8021f089e0a71e42d7a5cef388e0f66a38
vitalemonate <952070377@qq.com>
2021-04-28 14:43:05 +0800

    complete code

--- 2018_CVPR_MobileNetV2/code/model.py
+++ 2018_CVPR_MobileNetV2/code/model.py
@@ -2,6 +2,22 @@ import torch
 import torch.nn as nn
 
 
+def make_divisible(ch, divisor=8, min_ch=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_ch is None:
+        min_ch = divisor
+    new_ch = max(min_ch, int(ch + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_ch < 0.9 * ch:
+        new_ch += divisor
+    return new_ch
+
+
 class ConvBNReLU(nn.Sequential):
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, groups=1):
         padding = (kernel_size - 1) // 2
@@ -20,7 +36,7 @@ class InvertedResidual(nn.Module):
 
         layers = []
         if expand_ratio != 1:
-            layers.append(ConvBNReLU(in_channels, out_channels, kernel_size=1))
+            layers.append(ConvBNReLU(in_channels, hidden_channels, kernel_size=1))
 
         layers.extend([
             ConvBNReLU(hidden_channels, hidden_channels, stride=stride, groups=hidden_channels),
@@ -36,3 +52,73 @@ class InvertedResidual(nn.Module):
         else:
             x = self.conv(x)
         return x
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes=1000, alpha=1.0, round_nearest=8):
+        super(MobileNetV2, self).__init__()
+
+        input_channels = make_divisible(32*alpha, round_nearest)
+        last_channels = make_divisible(1280*alpha, round_nearest)
+
+        inverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1]
+        ]
+
+        features = []
+        features.append(ConvBNReLU(3, input_channels, stride=2))
+        for t, c, n, s in inverted_residual_setting:
+            output_channels = make_divisible(c*alpha, round_nearest)
+            for i in range(n):
+                if i == 0:
+                    features.append(InvertedResidual(input_channels, output_channels, stride=s, expand_ratio=t))
+                else:
+                    features.append(InvertedResidual(input_channels, output_channels, stride=1, expand_ratio=t))
+                input_channels = output_channels
+        features.append(ConvBNReLU(input_channels, last_channels, kernel_size=1))
+
+        self.features = nn.Sequential(*features)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifer = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(last_channels, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifer(x)
+        return x
+
+
+if __name__ == "__main__":
+    from torchsummary import summary
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = MobileNetV2().to(device)
+    summary(model, (3, 224, 224), device=str(device))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2018_CVPR_MobileNetV2/code/predict.py
@@ -0,0 +1,75 @@
+import json
+
+import torch
+import os
+import torchvision
+from torchvision import transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from model import MobileNetV2
+import numpy as np
+
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    data_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    # batch predict
+    images_path = "../../test_images"
+    dirs = os.listdir(images_path)
+    image_list = []
+    for img_name in dirs:
+        img_path = os.path.join(images_path, img_name)
+        img = Image.open(img_path)
+        img = data_transform(img)
+        image_list.append(img)
+
+    batch_image = torch.stack(image_list, dim=0)
+
+    json_path = "./class_indices.json"
+    assert os.path.exists(json_path), f"{json_path} not exists"
+
+    json_file = open(json_path, "r")
+    class_indict = json.load(json_file)
+
+    net = MobileNetV2(num_classes=5).to(device)
+    weight_path = "./MobileNetV2.pth"
+    net.load_state_dict(torch.load(weight_path, map_location=device))
+
+    net.eval()
+    with torch.no_grad():
+        output = torch.squeeze(net(batch_image.to(device)).cpu())
+        predict = torch.softmax(output, dim=1)
+        probs, classes = torch.max(predict, dim=1)
+
+        for idx, (pro, cla) in enumerate(zip(probs, classes)):
+            print("image: {}  class: {}  prob: {:.3}".format(dirs[idx],
+                                                             class_indict[str(cla.numpy())],
+                                                             pro.numpy()))
+        # visualisation
+        plot_show = torchvision.utils.make_grid(batch_image)
+        imshow(plot_show, title=[class_indict[str(x.numpy())] for x in classes])
+
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+if __name__ == "__main__":
+    main()
